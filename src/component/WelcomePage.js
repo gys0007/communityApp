679011fdaa8c50ea3765336ad668c9cd6dd47f90
Murrrrr <41497682+Murrrrr@users.noreply.github.com>
2022-04-30 16:32:42 +0900

    gfdg

--- src/component/WelcomePage.js
+++ src/component/WelcomePage.js
@@ -2,17 +2,21 @@ import React from 'react';
 import './WelcomePage.scss';
 
 const WelcomePage = () => {
+    let typingBool = false;
+    let typingIdx = 0;
+
+    let typingTxt = document.querySelector('.typing-txt').innerText;
     return(
         <div className="WelcomePage">
-            <p>PROTOCOL : LAP-M</p>
-            <p>COMPRESSION : V.42BIS</p>
-            <p>CONNECT 19200/ARQ\n</p>
+            <p className="typing-txt">PROTOCOL : LAP-M</p>
+            <p className="typing-txt">COMPRESSION : V.42BIS</p>
+            <p className="typing-txt">CONNECT 19200/ARQ\n</p>
             <br/>
-            <p>trying : baekdu</p>
-            <p>Connecting using IP ... Connected session 1</p>
+            <p className="typing-txt">trying : baekdu</p>
+            <p className="typing-txt">Connecting using IP ... Connected session 1</p>
             <br/>
-            <p>'나우누리'에 오신 것을 환영합니다.(baekdu pts/24)!!</p>
-            <p>나우 ID가 없는 분은 '손님' 혹은 'GUEST'를 입력하십시오.</p>
+            <p className="typing-txt">'나우누리'에 오신 것을 환영합니다.(baekdu pts/24)!!</p>
+            <p className="typing-txt">나우 ID가 없는 분은 '손님' 혹은 'GUEST'를 입력하십시오.</p>
         </div>
     )
 }
